d47e3f8d98caeaeed476a48bec065760ea0ba66a
JoannaL-24 <joanna.lin.lin0824@gmail.com>
2021-08-16 14:53:43 +0800

    Create ex2.py

---
+++ ex2.py
@@ -0,0 +1,4 @@
+# This is comment, people use it for clarifications and explainations about the function of the code
+
+print("I could have code like this.") # computer doesn't read comments
+print("This will run.")
