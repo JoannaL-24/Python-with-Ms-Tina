bc1662f07fb7928f0d09ac0a9df7c6f50b17b755
JoannaL-24 <joanna.lin.lin0824@gmail.com>
2021-08-18 14:09:07 +0800

    Update ex3.py

---
+++ ex3.py
@@ -0,0 +1,11 @@
+#calculate the speed of light
+print("3 * 10^8 = ", end="")
+c = 3 * 10**8
+print(c, "m/s")
+#calculate the wavelength of yellow light
+print("560 * 10^-9 ", end="")
+wavelength = 560 * 10**(-9)
+print("=", wavelength, "m")
+#calculate the frequency of the light
+print("the frequncy of 560 nm wavelength wave is", c/wavelength, " Hz")
+print("1/8 =", 1/8)
