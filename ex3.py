9fa61430564efe37eaef62a40a51a7146907ed92
JoannaL-24 <joanna.lin.lin0824@gmail.com>
2021-08-19 15:30:25 +0800

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,11 +1,51 @@
-#calculate the speed of light
+#tell the terminal to print(display) "I will now count my chickens:"
+print("I will now count my chickens:")
+
+#print "Hens" and the result after calculating (25+30/6)
+print("Hens", 25 + 30 / 6)
+#print "Hens" and the result after calculating 100-25*3%4
+#3 % 4 means the remainder from dividing 3 with 4
+print("Roosters", 100 - 25 * 3 % 4)
+
+#print "Now I will count the eggs:" in the terminal
+print("Now I will count the eggs:")
+
+#calculate 3+2+1-5+ 4%2 - 1/4 +6 and print the result
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+#display "Is it true that 3 + 2 < 5 - 7?" in the terminal
+print("Is it true that 3 + 2 < 5 - 7?")
+#check if (3+2) < (5-7) and print the result either True or False
+print(3 + 2 < 5 - 7)
+
+#print "What is 3 + 2?" and the result after calculating (3+2)
+print("What is 3 + 2?", 3 + 2)
+#print "What is 5 - 7?" and the result after calculating (5-7)
+print("What is 5 - 7?", 5 - 7)
+
+#print "Oh, that's why it's False." in the terminal
+print("Oh, that's why it's False.")
+#print "How about some more." in the terminal
+print("How about some more.")
+
+#print "Is it greater?" in the terminal and the result of whether 5 > -2 in True or False
+print("Is it greater?", 5 > -2)
+#print "Is it greater or equal?" and the result of whether 5 >= -2 in True or False
+print("Is it greater or equal?", 5 >= -2)
+#print "Is it less or equal?" and the result of whether 5 <= -2 in True or False
+print("Is it less or equal?", 5 <= -2)
+
+#print "3 * 10^8 = " without the enter key behind
 print("3 * 10^8 = ", end="")
+#calculate the speed of light
 c = 3 * 10**8
 print(c, "m/s")
-#calculate the wavelength of yellow light
+
 print("560 * 10^-9 ", end="")
+#calculate the wavelength of yellow light
 wavelength = 560 * 10**(-9)
 print("=", wavelength, "m")
+
 #calculate the frequency of the light
 print("the frequncy of 560 nm wavelength wave is", c/wavelength, " Hz")
+#testing out floating point (decimal)
 print("1/8 =", 1/8.0)
