0d73c627d4e34938a1050238a286ee559fe10d8e
JoannaL-24 <joanna.lin.lin0824@gmail.com>
2021-08-19 14:36:20 +0800

    Create ex7.py

---
+++ ex7.py
@@ -0,0 +1,38 @@
+#tell the terminal to display "Mary had a little lamb." with an enter key pressed(make the cursor to the nextline)
+print("Mary had a little lamb.")
+#tell the terminal to display "Its fleece was white as {}.", where {} is formatted and filled with the second string in the line 'snow'
+print("Its fleece was white as {}.".format('snow'))
+#tell the terminal to display "And everywhere that Mary went." with an enter key pressed
+print("And everywhere that Mary went.")
+#print "." 10 times, which results ".........."
+print("." * 10)
+
+#assign "C" to a variable called end1
+end1 = "C"
+#assign "h" to a variable called end2
+end2 = "h"
+#assign "e" to a variable called end3
+end3 = "e"
+#assign "e" to a variable called end4
+end4 = "e"
+#assign "s" to a variable called end5
+end5 = "s"
+#assign "e" to a variable called end6
+end6 = "e"
+#assign "B" to a variable called end7
+end7 = "B"
+#assign "u" to a variable called end8
+end8 = "u"
+#assign "r" to a variable called end9
+end9 = "r"
+#assign "g" to a variable called end10
+end10 = "g"
+#assign "e" to a variable called end11
+end11 = "e"
+#assign "r" to a variable called end12
+end12 = "r"
+
+#print the combination of end1, end2, end3, end4, end5, and end6, resulting "Cheese " without the enter pressed because of the "end=''" in the end of the line
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+#print the combination of end7, end8, end9, end10, end11, and end12, resulting "Burger"
+print(end7 + end8 + end9 + end10 + end11 + end12)
