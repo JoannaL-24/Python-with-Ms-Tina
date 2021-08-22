a29303f5563f1fbf86fbc82adca52a916ad0accd
JoannaL-24 <joanna.lin.lin0824@gmail.com>
2021-08-22 17:23:18 +0800

    Create ex12.py

---
+++ ex12.py
@@ -0,0 +1,12 @@
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weight? ")
+
+print("So you're %r old, %r tall and %r heavy." % (age, height, weight))
+
+"""
+command for pydoc in windows: py -m pydoc <name>
+sys has argv which allow us to read further argument when the program is run
+input takes any input and save the input as a string
+files are opened with open(filename, mode, etc...)
+"""
