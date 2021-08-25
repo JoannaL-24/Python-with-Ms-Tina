d02a789ada174a1e45fac58472b7e529bd9c9942
JoannaL-24 <joanna.lin.lin0824@gmail.com>
2021-08-25 22:21:18 +0800

    Create ex17.py

---
+++ ex17.py
@@ -0,0 +1,28 @@
+import shutil
+from sys import argv
+from os.path import exists
+from cmath import sqrt, log10
+from shutil import copy2
+
+script, from_file, to_file = argv
+
+print("Cpoying from %s to %s" %(from_file, to_file))
+
+infile = open(from_file)
+indata = infile.read()
+
+print("The input file is %d byte lone" % len(indata))
+
+print("Does the output file exist? %r" % exists(to_file))
+
+outfile = open(to_file,'w')
+outfile.write(indata)
+
+print("Alright, all done.")
+
+outfile.close()
+infile.close()
+
+#done in one line
+#shutil.copy2(from_file,to_file)
+#we have to close the file or else it would not save the writen data
