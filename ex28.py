bafeb2da93dcbe3bbd46f454a077b2048e224720
JoannaL-24 <joanna.lin.lin0824@gmail.com>
2021-09-02 14:14:01 +0800

    Create ex28.py

---
+++ ex28.py
@@ -0,0 +1,65 @@
+#learn boolean(True & False) logic
+print("True and True: ", (True and True))
+print("False and True: ", (False and True))
+print("1 == 1 and 2 == 1: ", (1 == 1 and 2 == 1))
+print("\"test\" == \"test\": ", ("test" == "test"))
+print("1 == 1 or 2 != 1: ", (1 == 1 or 2 != 1))
+print("True and 1 == 1: ", (True and 1 == 1))
+print("False and 0 != 0: ", (False and 0 != 0))
+print("True or 1 == 1: ", (True or 1 == 1))
+print("\"test\" == \"testing\": ", ("test" == "testing"))
+print("1 != 0 and 2 == 1: ", (1 != 0 and 2 == 1))
+print("\"test\" != \"testing\": ", ("test" != "testing"))
+print("\"test\" == 1: ", ("test" == 1))
+print("not(True and False): ", (not(True and False)))
+print("not(1 == 1 and 0 !=1): ", (not(1 == 1 and 0 !=1)))
+print("not(10 == 1 or 1000 == 1000): ", (not(10 == 1 or 1000 == 1000)))
+print("not(1 != 10 or 3 == 4): ", (not(1 != 10 or 3 == 4)))
+print("not(\"testing\" == \"testing\" and \"Zed\" == \"Cool Guy\"): ", (not("testing" == "testing" and "Zed" == "Cool Guy")))
+print("1 == 1 and not (\"testing\" == 1 or 1 == 0): ", (1 == 1 and not ("testing" == 1 or 1 == 0)))
+print("\"chunky\" == \"bacon\" and not (3 == 4 or 3 == 3): ", ("chunky" == "bacon" and not (3 == 4 or 3 == 3)))
+print("3 == 3 and not (\"testing\" == \"testing\" or \"Python\" == \"Fun\"): ", (3 == 3 and not ("testing" == "testing" or "Python" == "Fun")))
+
+"""
+Guesses for 1 to 20
+1. T
+2. F
+3. F
+4. T
+5. T
+6. T
+7. F
+8. T
+9. F
+10. F
+11. T
+12. F
+13. T
+14. F
+15. F
+16. F
+17. T
+18. T
+19. F
+20. F
+"""
+
+""""
+Study Drill:
+1. equality operations: ==, !=, >, <, >=, <=, and(&), or(|),Xor(^) not, is, is not, in, not in
+2.
+== equal
+!= not equal
+> greater than
+< less than
+>= greater than or equal to
+<= less than or equal to
+and and
+or or
+Xor xor
+not not
+is is 
+is not is not
+in in 
+not in not in
+"""
