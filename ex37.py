8e6736dab3975cced966baf1e5c54dbc9fed8611
JoannaL-24 <joanna.lin.lin0824@gmail.com>
2021-09-06 18:12:16 +0800

    Create ex37.py

---
+++ ex37.py
@@ -0,0 +1,27 @@
+# Initialize fitting parameters
+initial_theta = np.zeros(n+1)
+
+cost, grad = costFunction(initial_theta, X, y)
+
+print('Cost at initial theta (zeros): {:.3f}'.format(cost))
+print('Expected cost (approx): 0.693\n')
+
+print('Gradient at initial theta (zeros):')
+print('\t[{:.4f}, {:.4f}, {:.4f}]'.format(*grad))
+print('Expected gradients (approx):\n\t[-0.1000, -12.0092, -11.2628]\n')
+
+# Compute and display cost and gradient with non-zero theta
+test_theta = np.array([-24, 0.2, 0.2])
+cost, grad = costFunction(test_theta, X, y)
+
+print('Cost at test theta: {:.3f}'.format(cost))
+print('Expected cost (approx): 0.218\n')
+
+print('Gradient at test theta:')
+print('\t[{:.3f}, {:.3f}, {:.3f}]'.format(*grad))
+print('Expected gradients (approx):\n\t[0.043, 2.566, 2.647]')
+
+
+"""
+This is the code I chose, the flow chart is on Google classroom.
+"""
